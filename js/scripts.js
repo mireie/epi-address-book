fba7d40655de810ac460761df1b1d93777ac47d2
Franz Knupfer <franzknupfer@hotmail.com>
2020-12-02 11:38:38 -0800

    add methods for finding and deleting contacts

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 // Business Logic for AddressBook ---------
 function AddressBook() {
   this.contacts = {};
+  this.currentId = 0;
 }
 
 AddressBook.prototype.addContact = function(contact) {
@@ -11,6 +12,21 @@ AddressBook.prototype.addContact = function(contact) {
 AddressBook.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+  if (this.contacts[id] != undefined) {
+    return this.contacts[id];
+  }
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  if (this.contacts[id] === undefined) {
+    return false;
+  }
+  delete this.contacts[id];
+  return true;
 }
 
 // Business Logic for Contacts ---------
